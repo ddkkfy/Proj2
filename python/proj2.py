241e60b8099ec8f8343edb999f2643328b7fc465
TonyJ <haiyangj996@gmail.com>
2022-03-07 22:12:28 -0800

    increase heap to 0xfffff000

--- python/proj2.py
+++ python/proj2.py
@@ -48,7 +48,7 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--in_features', type=int, default=10, help="Input feature of Linear layer")
-    parser.add_argument('--out_features', type=int, default=30,  help="Output feature of Linear layer")
+    parser.add_argument('--out_features', type=int, default=2048,  help="Output feature of Linear layer")
     parser.add_argument('--batch', type=int, default=5, help="Input batch size")
 
     args = parser.parse_args()
